b014826290ca970751e33a917a46bc313f686317
Youngjin Jin <ijinjin@kaist.ac.kr>
2019-11-09 23:53:57 +0900

    compress code

--- s2cnn/s2_op.py
+++ s2cnn/s2_op.py
@@ -1,11 +1,108 @@
 # pylint: disable=R,C,E1101
-from functools import lru_cache
 import torch
 import torch.cuda
+import numpy as np
 from string import Template
+from functools import lru_cache
+from s2cnn.utils.decorator import cached_dirpklgz
 
-# TODO simplify the cuda code like it was done in SO3_mm using only one code for the kernel
+# s2_ft.py
 
+def s2_rft(x, b, grid):
+    """
+    Real Fourier Transform
+    :param x: [..., beta_alpha]
+    :param b: output bandwidth signal
+    :param grid: tuple of (beta, alpha) tuples
+    :return: [l * m, ..., complex]
+    """
+    # F is the Fourier matrix
+    F = _setup_s2_ft(b, grid, device_type=x.device.type, device_index=x.device.index)  # [beta_alpha, l * m, complex]
+
+    assert x.size(-1) == F.size(0)
+
+    sz = x.size()
+    x = torch.einsum("ia,afc->fic", (x.view(-1, x.size(-1)), F.clone()))  # [l * m, ..., complex]
+    x = x.view(-1, *sz[:-1], 2)
+    return x
+
+
+@cached_dirpklgz("cache/setup_s2_ft")
+def __setup_s2_ft(b, grid):
+    from lie_learn.representations.SO3.wigner_d import wigner_D_matrix
+
+    # Note: optionally get quadrature weights for the chosen grid and use them to weigh the D matrices below.
+    # This is optional because we can also view the filter coefficients as having absorbed the weights already.
+
+    # Sample the Wigner-D functions on the local grid
+    n_spatial = len(grid)
+    n_spectral = np.sum([(2 * l + 1) for l in range(b)])
+    F = np.zeros((n_spatial, n_spectral), dtype=complex)
+    for i, (beta, alpha) in enumerate(grid):
+        Dmats = [(2 * b) * wigner_D_matrix(l, alpha, beta, 0,
+                                           field='complex', normalization='quantum', order='centered', condon_shortley='cs')
+                 .conj()
+                 for l in range(b)]
+        F[i] = np.hstack([Dmats[l][:, l] for l in range(b)])
+
+    # F is a complex matrix of shape (n_spatial, n_spectral)
+    # If we view it as float, we get a real matrix of shape (n_spatial, 2 * n_spectral)
+    # In the so3_local_ft, we will multiply a batch of real (..., n_spatial) vectors x with this matrix F as xF.
+    # The result is a (..., 2 * n_spectral) array that can be interpreted as a batch of complex vectors.
+    F = F.view('float').reshape((-1, n_spectral, 2))
+    return F
+
+
+@lru_cache(maxsize=32)
+def _setup_s2_ft(b, grid, device_type, device_index):
+    F = __setup_s2_ft(b, grid)
+
+    # convert to torch Tensor
+    F = torch.tensor(F.astype(np.float32), dtype=torch.float32, device=torch.device(device_type, device_index))  # pylint: disable=E1102
+
+    return F
+
+# s2_grid.py
+
+def s2_near_identity_grid(max_beta=np.pi / 8, n_alpha=8, n_beta=3):
+    '''
+    :return: rings around the north pole
+    size of the kernel = n_alpha * n_beta
+    '''
+    beta = np.arange(start=1, stop=n_beta + 1, dtype=np.float) * max_beta / n_beta
+    alpha = np.linspace(start=0, stop=2 * np.pi, num=n_alpha, endpoint=False)
+    B, A = np.meshgrid(beta, alpha, indexing='ij')
+    B = B.flatten()
+    A = A.flatten()
+    grid = np.stack((B, A), axis=1)
+    return tuple(tuple(ba) for ba in grid)
+
+
+def s2_equatorial_grid(max_beta=0, n_alpha=32, n_beta=1):
+    '''
+    :return: rings around the equator
+    size of the kernel = n_alpha * n_beta
+    '''
+    beta = np.linspace(start=np.pi/2 - max_beta, stop=np.pi/2 + max_beta, num=n_beta, endpoint=True)
+    alpha = np.linspace(start=0, stop=2 * np.pi, num=n_alpha, endpoint=False)
+    B, A = np.meshgrid(beta, alpha, indexing='ij')
+    B = B.flatten()
+    A = A.flatten()
+    grid = np.stack((B, A), axis=1)
+    return tuple(tuple(ba) for ba in grid)
+
+
+def s2_soft_grid(b):
+    beta = (np.arange(2 * b) + 0.5) / (2 * b) * np.pi
+    alpha = np.linspace(start=0, stop=2 * np.pi, num=2 * b, endpoint=False)
+    B, A = np.meshgrid(beta, alpha, indexing='ij')
+    B = B.flatten()
+    A = A.flatten()
+    grid = np.stack((B, A), axis=1)
+    return tuple(tuple(ba) for ba in grid)
+
+
+# s2_mm.py
 
 def s2_mm(x, y):
     '''
--- s2cnn/so3_op.py
+++ s2cnn/so3_op.py
@@ -1,9 +1,125 @@
 # pylint: disable=R,C,E1101
-import math
-from functools import lru_cache
 import torch
 import torch.cuda
+import numpy as np
+import math
+import warnings
+from functools import lru_cache
+from s2cnn.utils.decorator import cached_dirpklgz
 
+# so3_ft.py
+
+def so3_rft(x, b, grid):
+    """
+    Real Fourier Transform
+    :param x: [..., beta_alpha_gamma]
+    :param b: output bandwidth signal
+    :param grid: tuple of (beta, alpha, gamma) tuples
+    :return: [l * m * n, ..., complex]
+    """
+    # F is the Fourier matrix
+    F = _setup_so3_ft(b, grid, device_type=x.device.type, device_index=x.device.index)  # [beta_alpha_gamma, l * m * n, complex]
+
+    assert x.size(-1) == F.size(0)
+
+    sz = x.size()
+    x = torch.einsum("ia,afc->fic", (x.view(-1, x.size(-1)), F.clone()))  # [l * m * n, ..., complex]
+    x = x.view(-1, *sz[:-1], 2)
+    return x
+
+
+@cached_dirpklgz("cache/setup_so3_ft")
+def __setup_so3_ft(b, grid):
+    from lie_learn.representations.SO3.wigner_d import wigner_D_matrix
+
+    # Note: optionally get quadrature weights for the chosen grid and use them to weigh the D matrices below.
+    # This is optional because we can also view the filter coefficients as having absorbed the weights already.
+    # The weights depend on the spacing between the point of the grid
+    # Only the coefficient sin(beta) can be added without requireing to know the spacings
+
+    # Sample the Wigner-D functions on the local grid
+    n_spatial = len(grid)
+    n_spectral = np.sum([(2 * l + 1) ** 2 for l in range(b)])
+    F = np.zeros((n_spatial, n_spectral), dtype=complex)
+    for i, (beta, alpha, gamma) in enumerate(grid):
+        Dmats = [wigner_D_matrix(l, alpha, beta, gamma,
+                                 field='complex', normalization='quantum', order='centered', condon_shortley='cs')
+                 .conj()
+                 for l in range(b)]
+        F[i] = np.hstack([Dl.flatten() for Dl in Dmats])
+
+    # F is a complex matrix of shape (n_spatial, n_spectral)
+    # If we view it as float, we get a real matrix of shape (n_spatial, 2 * n_spectral)
+    # In the so3_local_ft, we will multiply a batch of real (..., n_spatial) vectors x with this matrix F as xF.
+    # The result is a (..., 2 * n_spectral) array that can be interpreted as a batch of complex vectors.
+    F = F.view('float').reshape((-1, n_spectral, 2))
+    return F
+
+
+@lru_cache(maxsize=32)
+def _setup_so3_ft(b, grid, device_type, device_index):
+    F = __setup_so3_ft(b, grid)
+
+    # convert to torch Tensor
+    F = torch.tensor(F.astype(np.float32), dtype=torch.float32, device=torch.device(device_type, device_index))  # pylint: disable=E1102
+
+    return F
+
+
+# so3_grid.py
+
+def so3_near_identity_grid(max_beta=np.pi / 8, max_gamma=2*np.pi, n_alpha=8, n_beta=3, n_gamma=None):
+    '''
+    :return: rings of rotations around the identity, all points (rotations) in
+    a ring are at the same distance from the identity
+    size of the kernel = n_alpha * n_beta * n_gamma
+    '''
+    if n_gamma is None:
+        n_gamma = n_alpha # similar to regular representations
+    beta = np.arange(start=1, stop=n_beta + 1, dtype=np.float) * max_beta / n_beta
+    alpha = np.linspace(start=0, stop=2 * np.pi, num=n_alpha, endpoint=False)
+    pre_gamma = np.linspace(start=-max_gamma, stop=max_gamma, num=n_gamma, endpoint=True)
+    B, A, preC = np.meshgrid(beta, alpha, pre_gamma, indexing='ij')
+    C = preC - A
+    B = B.flatten()
+    A = A.flatten()
+    C = C.flatten()
+    grid = np.stack((B, A, C), axis=1)
+    if sum(grid[:, 0] == 0) > 1:
+        warnings.warn("Gimbal lock: beta take value 0 in the grid")
+    return tuple(tuple(bac) for bac in grid)
+
+
+def so3_equatorial_grid(max_beta=0, max_gamma=np.pi / 8, n_alpha=32, n_beta=1, n_gamma=2):
+    '''
+    :return: rings of rotations around the equator.
+    size of the kernel = n_alpha * n_beta * n_gamma
+    '''
+    beta = np.linspace(start=np.pi/2 - max_beta, stop=np.pi/2 + max_beta, num=n_beta, endpoint=True)
+    alpha = np.linspace(start=0, stop=2 * np.pi, num=n_alpha, endpoint=False)
+    gamma = np.linspace(start=-max_gamma, stop=max_gamma, num=n_gamma, endpoint=True)
+    B, A, C = np.meshgrid(beta, alpha, gamma, indexing='ij')
+    B = B.flatten()
+    A = A.flatten()
+    C = C.flatten()
+    grid = np.stack((B, A, C), axis=1)
+    if sum(grid[:, 0] == 0) > 1:
+        warnings.warn("Gimbal lock: beta take value 0 in the grid")
+    return tuple(tuple(bac) for bac in grid)
+
+
+def so3_soft_grid(b):
+    beta = (np.arange(2 * b) + 0.5) / (2 * b) * np.pi
+    alpha = gamma = np.linspace(start=0, stop=2 * np.pi, num=2 * b, endpoint=False)
+    B, A, C = np.meshgrid(beta, alpha, gamma, indexing='ij')
+    B = B.flatten()
+    A = A.flatten()
+    C = C.flatten()
+    grid = np.stack((B, A, C), axis=1)
+    return tuple(tuple(bac) for bac in grid)
+
+
+# so3_mm.py
 
 def so3_mm(x, y):
     '''
